afe424e75bf5e35b4a97ace3d9cb39f7ae2ff8eb
Cemal Burak Aygün <cburakaygun@gmail.com>
2018-04-10 23:42:29 +0300

    Create initial Api structure

--- twitterApiProject/Api/views.py
+++ twitterApiProject/Api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class memoryPosts(APIView):
+	def get(self, request):
+		params = request.query_params
+		type = params['type']
+		if type == 'all':
+			return self.all_posts()
+		elif type == 'search':
+			return self.search_posts(params)
+		else:
+			pass # ERROR
+
+	def search_posts(self, params):
+		# Handle searching for specific Tweets here.
+		result = {'test_entry': 'searching_tweets'}	# Populate this
+		return Response(result)
+
+	def all_posts(self):
+		# Handle retrieving all the Tweets here.
+		result = {'test_entry': 'all_tweets'}	# Populate this
+		return Response(result)
+
+	def post(self, request):
+		# Handle creating a new Tweet here.
+		result = {'test_entry': 'creating_tweets'}	# Populate this
+		return Response(result)
--- twitterApiProject/twitterApiProject/urls.py
+++ twitterApiProject/twitterApiProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from Api import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/memory_posts/', views.memoryPosts.as_view()),
 ]
