2905a8ddcc5323bb4d76bb7ff7bbdbed0549325d
Ramazan Arslan <ramazan@birlikte.al>
2018-04-13 19:46:47 +0300

    deleted model and form py files. Because they are not necessary

--- twitterApiProject/Api/forms.py
+++
@@ -1,9 +0,0 @@
-from  django import forms
-
-from .models import *
-
-
-class CreateTweet(forms.ModelForm):
-    class Meta:
-        model = CreateTweet
-        fields = ('story', 'username','tags')
