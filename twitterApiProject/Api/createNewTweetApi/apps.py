e636ed27c6dc8c389989801c59b7934b8bce9fe0
Cemal Burak Aygün <cburakaygun@gmail.com>
2018-04-13 19:38:32 +0300

    Delete unnecessary files

--- twitterApiProject/Api/createNewTweetApi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CreatenewtweetapiConfig(AppConfig):
-    name = 'createNewTweetApi'
--- twitterApiProject/Ui/retrieveTweetUi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RetrievetweetuiConfig(AppConfig):
-    name = 'retrieveTweetUi'
