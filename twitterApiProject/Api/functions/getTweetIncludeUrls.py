dae06ccb9da8b9f13b5271c66133eaca27b88d21
ramazanarslan <31334024+ramazanarslan@users.noreply.github.com>
2018-04-14 14:21:29 +0300

    updated getTweetIncludeUrls

--- twitterApiProject/Api/functions/getTweetIncludeUrls.py
+++ twitterApiProject/Api/functions/getTweetIncludeUrls.py
@@ -9,9 +9,12 @@ def getTweetIncludeUrls():
     auth.set_access_token(TwitterKeys.ACCESS_TOKEN, TwitterKeys.ACCESS_SECRET)
 
     api = tweepy.API(auth)
-    public_tweets = api.home_timeline()
-    filterOnlyUrls=[]
-    for tweet in public_tweets:
+    userTweets = tweepy.Cursor(api.user_timeline, id='history_g7').items()
+
+    filterOnlyUrls = []
+
+    for tweet in userTweets:
+        print(tweet.text.encode('utf-8'))
         if  str(tweet.text).__contains__("http"):
             filterOnlyUrls.append(tweet.text)
     return filterOnlyUrls
