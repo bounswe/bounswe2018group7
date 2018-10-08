d94844de6070806d4c9f1f830359418b8201dd2e
Cemal Burak Aygün <cburakaygun@gmail.com>
2018-10-08 22:41:11 +0300

    Revise api.urls and api.models.User

--- app/backend/hiStoryBackend/api/urls.py
+++ app/backend/hiStoryBackend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.conf.urls import url
 
 from .views.auth import views_v1 as auth_views_v1
 from .views.memory_post import views_v1 as memory_post_views_v1
@@ -7,9 +7,9 @@ app_name = 'api'
 
 urlpatterns = [
     # AUTH
-    path('v1/signin/', auth_views_v1.sign_in),
-    path('v1/signup/', auth_views_v1.sign_up),
+    url(r'^v1/signin/?$', auth_views_v1.sign_in),
+    url(r'^v1/signup/?$', auth_views_v1.sign_up),
 
     # Memory Posts
-    path('v1/memory_posts/', memory_post_views_v1.response),
+    url(r'^v1/memory_posts/?$', memory_post_views_v1.response),
 ]
