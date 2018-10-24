75a75dca9d07dbc644f349a6f32d067f48ae9684
Cemal Burak Aygün <cburakaygun@gmail.com>
2018-10-24 17:47:54 +0300

    Refactor api.helpers.custom_helpers.errors_from_dict_to_arr(errors_dict)

--- app/backend/hiStoryBackend/api/helpers/custom_helpers.py
+++ app/backend/hiStoryBackend/api/helpers/custom_helpers.py
@@ -5,13 +5,7 @@ def errors_from_dict_to_arr(errors_dict):
     """
     non_field_errors_arr = errors_dict.pop('non_field_errors', [])
 
-    result = []
-
-    for key in errors_dict:
-        for msg in errors_dict[key]:
-            result.append(f"{key}: {msg}")
-
-    for msg in non_field_errors_arr:
-        result.append(msg)
+    result = [f"{key}: {msg}" for key in errors_dict for msg in errors_dict[key]]
+    result.extend([msg for msg in non_field_errors_arr])
 
     return result
