8b3c8fc4e649906220a26d480a1b5fdc38fa4068
Cemal Burak Aygün <cburakaygun@gmail.com>
2018-10-24 17:20:30 +0300

    Add 'password_reset_token' field to api.models.User

--- app/backend/hiStoryBackend/api/models.py
+++ app/backend/hiStoryBackend/api/models.py
@@ -26,6 +26,15 @@ class User(AbstractUser):
         default=None
     )
 
+    password_reset_token = models.CharField(
+        max_length=40,
+        unique=True,
+        db_index=True,
+        null=True,
+        blank=True,
+        default=None
+    )
+
     @property
     def admin(self):
         return self.is_superuser
@@ -43,15 +52,19 @@ class User(AbstractUser):
         self.is_active = not ban
         self.save(update_fields=['is_active'])
 
-    def create_confirmation_token(self):
+    def create_token_for(self, token_field_name):
         """
-        Creates an email confirmation token for this User and saves to database.
+        Creates an email confirmation token or password reset token for this User and saves to database.
         :return: None
         """
         while True:
-            self.confirmation_token = Token().generate_key()
+            if token_field_name == 'confirmation_token':
+                self.confirmation_token = Token().generate_key()
+            else:
+                self.password_reset_token = Token().generate_key()
+
             try:
-                self.save(update_fields=['confirmation_token'])
+                self.save(update_fields=[token_field_name])
                 break
             except IntegrityError:  # If token is not unique
                 continue
@@ -59,10 +72,10 @@ class User(AbstractUser):
     def send_confirmation_email(self):
         """
         Creates an email confirmation token for this User and saves to database.
-        Then, sends an confirmation email to the User.
-        :return: True, if process is successsful. False, otherwise.
+        Then, sends a confirmation email to the User.
+        :return: True, if process is successful. False, otherwise.
         """
-        self.create_confirmation_token()
+        self.create_token_for('confirmation_token')
 
         text_message = render_to_string('../templates/auth/email_confirmation.txt',
                                         {'user': self, 'base_url': settings.FRONTEND_CONFIRMATION_BASE_URL})
@@ -72,6 +85,31 @@ class User(AbstractUser):
         try:
             sent_email_count = send_mail(
                 'hiStory - Email Confirmation',
+                text_message,
+                settings.GMAIL_USERNAME,
+                [self.email],
+                html_message=html_message
+            )
+            return sent_email_count != 0
+        except SMTPException:
+            return False
+
+    def send_password_reset_email(self):
+        """
+        Creates a password reset token for this User and saves to database.
+        Then, sends a password reset email to the User.
+        :return: True, if process is successful. False, otherwise.
+        """
+        self.create_token_for('password_reset_token')
+
+        text_message = render_to_string('../templates/auth/password_reset.txt',
+                                        {'user': self, 'base_url': settings.FRONTEND_PASSWORD_RESET_BASE_URL})
+        html_message = render_to_string('../templates/auth/password_reset.html',
+                                        {'user': self, 'base_url': settings.FRONTEND_PASSWORD_RESET_BASE_URL})
+
+        try:
+            sent_email_count = send_mail(
+                'hiStory - Password Reset',
                 text_message,
                 settings.GMAIL_USERNAME,
                 [self.email],
