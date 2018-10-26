d0886b32bbb2e370215e743d3fb82994b7ebb2b4
Bekir Burak Aslan <bekir@birlikte.al>
2018-10-26 18:48:38 +0300

    map is added

--- app/frontend/history/src/views/Components/Sections/AroundYou.jsx
+++ app/frontend/history/src/views/Components/Sections/AroundYou.jsx
@@ -3,7 +3,7 @@ import PTextInput from "components/PTextInput";
 import { withStyles } from "@material-ui/core/styles";
 import pillsStyle from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.jsx";
 import PComment from "components/PComment";
-
+import PMaps from "components/PMaps";
 class AroundYou extends Component {
   constructor(props) {
     super(props);
@@ -33,6 +33,7 @@ class AroundYou extends Component {
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
           }
         />
+        <PMaps />
       </div>
     );
   }
