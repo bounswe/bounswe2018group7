ba5727468af051787537760f24d9dfcef3d8cfca
Bekir Burak Aslan <bekir@birlikte.al>
2018-10-26 15:31:08 +0300

    Thumbnail is added

---
+++ app/frontend/history/src/components/PComment/index.js
@@ -0,0 +1,123 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import classnames from "classnames";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import red from "@material-ui/core/colors/red";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import PTextInput from "components/PTextInput";
+
+const styles = theme => ({
+  card: {
+    width: "100%"
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  actions: {
+    display: "flex"
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    }),
+    marginLeft: "auto",
+    [theme.breakpoints.up("sm")]: {
+      marginRight: -8
+    }
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+});
+
+class PComment extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes, title, MainText, SubText, companyName } = this.props;
+
+    return (
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={title}
+            subheader="September 14, 2016"
+          />
+          <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Contemplative Reptile" />
+          <CardContent>
+            <Typography component="p">{MainText}</Typography>
+          </CardContent>
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>{SubText}</Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+        <PTextInput
+          value={companyName}
+          onChange={event => this.setState({ companyName: event.target.value })}
+          label={"Your Comments"}
+          className={classes.dialogTextInput}
+          multiline
+          rows={5}
+          fullWidth
+        />
+      </div>
+    );
+  }
+}
+
+PComment.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(PComment);
--- app/frontend/history/src/views/Components/Sections/AroundYou.jsx
+++ app/frontend/history/src/views/Components/Sections/AroundYou.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PTextInput from "components/PTextInput";
 import { withStyles } from "@material-ui/core/styles";
 import pillsStyle from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.jsx";
+import PComment from "components/PComment";
 
 class AroundYou extends Component {
   constructor(props) {
@@ -25,8 +26,13 @@ class AroundYou extends Component {
           rows={5}
           fullWidth
         />
-
-        <h1>Ram</h1>
+        <PComment
+          title={"Bekir Burak ASLAN"}
+          MainText={"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."}
+          SubText={
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          }
+        />
       </div>
     );
   }
