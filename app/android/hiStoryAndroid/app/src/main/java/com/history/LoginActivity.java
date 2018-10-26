27868087e1684c18c27d83fa7ec3595fbff27780
serdarada <srdr38@gmail.com>
2018-10-26 19:24:30 +0300

    Created initial design of login activity

--- app/android/hiStoryAndroid/app/src/main/java/com/history/LoginActivity.java
+++ app/android/hiStoryAndroid/app/src/main/java/com/history/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.history;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+    }
+
+    public void openRegisterActivity(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+    public void skipToHomepage(View view){
+        Intent intent = new Intent(this, HomePageActivity.class);
+        startActivity(intent);
     }
 }
