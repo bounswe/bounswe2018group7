ab5f75ba00e9382ef0d5fb63dbafa721f2b88b51
Muhammed Fatih Balin <m.f.balin@gmail.com>
2018-10-27 22:36:50 +0300

    MemoryPostSerializer has been implemented, MemoryPostView post request has been implemented

--- app/backend/hiStoryBackend/api/serializers.py
+++ app/backend/hiStoryBackend/api/serializers.py
@@ -1,4 +1,7 @@
 from api.models import User
+from api.models import MemoryPost
+from api.models import MemoryMedia
+from django.core.files.base import File
 from rest_framework import serializers
 from api.helpers.custom_helpers import errors_from_dict_to_arr
 
@@ -54,3 +57,58 @@ class UserSerializer(CustomBaseSerializer):
         instance.save()
 
         return instance
+
+
+class MemoryPostSerializer(CustomBaseSerializer):
+	story = serializers.JSONField(required = False)
+	location = serializers.JSONField(required = False)
+	time = serializers.JSONField(required = False)
+
+	class Meta:
+		model = MemoryPost
+		exclude = ('user',)
+
+	def validate_json_array(self, field_name, field_value):
+		if isinstance(field_value, str):
+			try:
+				field_arr = json.loads(field_value)
+				if isinstance(field_arr, list):
+					return field_arr
+			except JSONDecodeError:
+				pass
+		elif isinstance(field_value, list):
+			return field_value
+
+		raise serializers.ValidationError({field_name: "must be an Array or a String representing a JSON-Array"})
+
+	def validate_time(self, value):
+		return self.validate_json_array('time', value)
+
+	def validate_location(self, value):
+		return self.validate_json_array('location', value)
+
+	def create(self, validated_data):
+		memory_post = MemoryPost(	user = validated_data['user'],
+									title = validated_data['title'],
+									story = [],
+									time = validated_data.get('time'),
+									location = validated_data.get('location'))
+		memory_post.save()
+		story = []
+		for i, item in enumerate(validated_data['story']):
+			if isinstance(item, File):
+				if not item.content_type.startswith('image') and not item.content_type.startswith('video') and not item.content_type.startswith('audio'):
+					continue
+				memory_media = MemoryMedia()
+				memory_media.memory_post = memory_post
+				memory_media.type = item.content_type
+				memory_media.order = i
+				memory_media.file = item
+				memory_media.save()
+				story.append({"type": item.content_type, "payload": memory_media.file.url})
+			else:
+				story.append({"type": "text", "payload": item})
+		memory_post.story = story
+		
+		memory_post.save()
+		return memory_post
