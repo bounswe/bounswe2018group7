71c601320ae1a0999f3e3d28411d5f2303bd923c
abcdrk <dilrubareyyan@outlook.com>
2018-10-30 11:20:23 +0300

    Add Create PostActivity.java

--- app/android/hiStoryAndroid/app/src/main/java/com/history/HomePageActivity.java
+++ app/android/hiStoryAndroid/app/src/main/java/com/history/HomePageActivity.java
@@ -1,7 +1,9 @@
 package com.history;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
 
