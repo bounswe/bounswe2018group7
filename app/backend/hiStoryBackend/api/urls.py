ef1d63dd7cb8aaab483819d583ed48e0eb54264f
Cemal Burak Aygün <cburakaygun@gmail.com>
2019-01-01 15:07:27 +0300

    Implement api/v1/recommendation endpoint

--- app/backend/hiStoryBackend/api/urls.py
+++ app/backend/hiStoryBackend/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
+from api.views.v1.recommendation_views import RecommendationView
 from .views.v1.reaction_views import ReactionView
 from .views.v1.comment_views import CommentView
 from .views.v1.auth_views import SignUpView, SignInView, SignOutView, EmailConfirmationView, PasswordResetView
@@ -21,5 +22,7 @@ urlpatterns = [
 
 	url(r'^v1/reactions/?(/(?P<id>\d+)/?)?$', ReactionView.as_view()),
 
-	url(r'^v1/profiles/?(/(?P<username>[^/]+)/?)?$', ProfileView.as_view())
+	url(r'^v1/profiles/?(/(?P<username>[^/]+)/?)?$', ProfileView.as_view()),
+
+	url(r'^v1/recommendation/?$', RecommendationView.as_view())
 ]
