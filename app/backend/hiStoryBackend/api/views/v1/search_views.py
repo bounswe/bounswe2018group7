9fa1d63aed3b2df5afe6618bef06781e9ba96028
Enes Koşar <eneskosar@hotmail.com.tr>
2019-01-01 23:01:03 +0300

    changed the endpoint of return

--- app/backend/hiStoryBackend/api/views/v1/search_views.py
+++ app/backend/hiStoryBackend/api/views/v1/search_views.py
@@ -16,40 +16,48 @@ class SearchView(APIView):
     def post(self, request):
         text = request.data.get('text')
 
-        mem_list = []
+        mem_list_id = []
+        mem_list_title = []
         user_list = []
 
         for post in MemoryPost.objects.all():
             if text in post.title:
-                if post.id not in mem_list:
-                    mem_list.append(post.id)
+                if post.id not in mem_list_id:
+                    mem_list_id.append(post.id)
+                    mem_list_title.append(post.title)
             for story in post.story:
                 if text in story['payload']:
-                    if post.id not in mem_list:
-                        mem_list.append(post.id)
+                    if post.id not in mem_list_id:
+                        mem_list_id.append(post.id)
+                        mem_list_title.append(post.title)
             if post.time:
                 if text in post.time['data']:
-                    if post.id not in mem_list:
-                        mem_list.append(post.id)
+                    if post.id not in mem_list_id:
+                        mem_list_id.append(post.id)
+                        mem_list_title.append(post.title)
             for tag in post.tags:
                 if text in tag:
-                    if post.id not in mem_list:
-                        mem_list.append(post.id)
+                    if post.id not in mem_list_id:
+                        mem_list_id.append(post.id)
+                        mem_list_title.append(post.title)
             for location in post.location:
                 if location['type'] == 'path':
                     for points in location['points']:
                         for point in points:
                             if text in point:
-                                if post.id not in mem_list:
-                                    mem_list.append(post.id)
+                                if post.id not in mem_list_id:
+                                    mem_list_id.append(post.id)
+                                    mem_list_title.append(post.title)
                 if location['type'] == 'region':
                     if text in location['name']:
-                        if post.id not in mem_list:
-                            mem_list.append(post.id)
+                        if post.id not in mem_list_id:
+                            mem_list_id.append(post.id)
+                            mem_list_title.append(post.title)
                 if location['type'] == 'point':
                     if text in location['point']:
-                        if post.id not in mem_list:
-                            mem_list.append(post.id)
+                        if post.id not in mem_list_id:
+                            mem_list_id.append(post.id)
+                            mem_list_title.append(post.title)
 
         
         for user in User.objects.all():
@@ -64,6 +72,6 @@ class SearchView(APIView):
                     user_list.append(user.username)
         
         return jrh.success(
-            {'memory_posts':  mem_list,
+            {'memory_posts':  { 'ids': mem_list_id, 'titles' : mem_list_title},
             'users': user_list}
     )
